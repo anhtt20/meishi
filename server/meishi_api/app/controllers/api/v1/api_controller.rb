d796a8c760154ffca183ed4eb093a1e5f619561e
Vo Xuan Hoang <xuanhoang1102@hotmail.com>
2016-08-16 10:22:40 +0900

    Update layout, Delete old database

--- server/meishi_api/app/controllers/api/v1/api_controller.rb
+++ server/meishi_api/app/controllers/api/v1/api_controller.rb
@@ -1,6 +1,6 @@
 module Api::V1
   class ApiController < ApplicationController
     # Generic API stuff here
-    before_action :authenticate_user!
+    #before_action :authenticate_user!
   end
 end
