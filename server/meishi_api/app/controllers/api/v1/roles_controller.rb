e0b625f022bcaa70fdb318af6c179ee4ef56d7df
Vo Xuan Hoang <xuanhoang1102@hotmail.com>
2016-08-16 17:56:29 +0900

    minify role controllers

--- server/meishi_api/app/controllers/api/v1/roles_controller.rb
+++ server/meishi_api/app/controllers/api/v1/roles_controller.rb
@@ -3,55 +3,10 @@ module Api::V1
     before_action :set_role, only: [:show, :update, :destroy]
 
     # GET /roles
-    def index
-      @roles = Role.all
-
-      render json: @roles
-    end
-
-    # GET /roles/1
-    def show
+    def get
+      @role = Role.find(params[:role_id])
+      
       render json: @role
-    end
-
-    # POST /roles
-    def create
-      @role = Role.new(role_params)
-
-      if @role.save
-        render json: @role, status: :created, location: @role
-      else
-        render json: @role.errors, status: :unprocessable_entity
-      end
-    end
-
-    # PATCH/PUT /roles/1
-    def update
-      if @role.update(role_params)
-        render json: @role
-      else
-        render json: @role.errors, status: :unprocessable_entity
-      end
-    end
-
-    # DELETE /roles/1
-    def destroy
-      @role.destroy
-    end
-
-    def current
-      render json: User.includes(:roles).find_by(:email => current_user.email)
-    end
-
-    private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_role
-      @role = Role.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def role_params
-      params.require(:role).permit(:name)
     end
   end
 end
